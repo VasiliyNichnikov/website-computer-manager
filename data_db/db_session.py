cb53b2434db4f0dae3f45bf5c260ab86fbb6af6f
vnich <v.nichnikov@gmail.com>
2020-04-21 23:01:07 +0300

    website

--- data_db/db_session.py
+++ data_db/db_session.py
@@ -1,7 +1,7 @@
 import psycopg2
 import sqlalchemy as sa
 import sqlalchemy.orm as orm
-from sqlalchemy.orm import Session, scoped_session, sessionmaker
+from sqlalchemy.orm import Session
 import sqlalchemy.ext.declarative as dec
 
 SqlAlchemyBase = dec.declarative_base()
@@ -23,7 +23,6 @@ def global_init():
     print(f"Подключение к базе данных по адресу {conn_str}")
 
     engine = sa.create_engine(conn_str, echo=False)
-    #  engine = "postgres://fincvilioztuwy:5529db1105625b5b8c56049348c7393e0e528d202c8a10bd0c0e22f1596a12d6@ec2-54-75-246-118.eu-west-1.compute.amazonaws.com:5432/d85u8gcgigior0"
     __factory = orm.sessionmaker(bind=engine)
 
     from . import __all_models
